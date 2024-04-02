ff19b593994196e7520f924264e8e6779907e1ab
Divyaraj Makwana <divyaraj.makwana9425@gmail.com>
2024-04-02 18:21:26 +0530

    shopping list add button work done, new item is added

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, OnInit, Output, EventEmitter, ElementRef } from '@angular/core';
+import { Ingredients } from '../../shared/ingredients.model';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
   styleUrl: './shopping-edit.component.css'
 })
-export class ShoppingEditComponent {
+export class ShoppingEditComponent implements OnInit {
+  @ViewChild('nameInput') nameInput: ElementRef;
+  @ViewChild('amountInput') amountInput: ElementRef;
+  @Output() ingredientAdded = new EventEmitter<Ingredients>();
 
+  onAddItem() {
+    const name = this.nameInput.nativeElement.value;
+    const amount = this.amountInput.nativeElement.value;
+    const ingredient = new Ingredients(name, amount);
+    this.ingredientAdded.emit(ingredient);
+  }
+  ngOnInit() { }
 }
