177e3a9c03cf7435ee55cf83efeabb5b05977968
Divyaraj Makwana <divyaraj.makwana9425@gmail.com>
2024-04-09 10:45:32 +0530

    shopping edit component has form and it is done with td approach of form

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,6 +1,7 @@
-import { Component, ViewChild, OnInit, ElementRef } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { Ingredients } from '../../shared/ingredients.model';
 import { ShoppingListService } from '../shopping-list.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -8,16 +9,17 @@ import { ShoppingListService } from '../shopping-list.service';
   styleUrl: './shopping-edit.component.css'
 })
 export class ShoppingEditComponent implements OnInit {
-  @ViewChild('nameInput') nameInput: ElementRef;
-  @ViewChild('amountInput') amountInput: ElementRef;
 
   constructor(private shoppingListService: ShoppingListService) { }
 
-  onAddItem() {
-    const name = this.nameInput.nativeElement.value;
-    const amount = this.amountInput.nativeElement.value;
-    const ingredient = new Ingredients(name, amount);
+  onAddItem(form: NgForm) {
+    const value = form.value
+    console.log(value);
+    const ingredient = new Ingredients(value.name, value.amount);
     this.shoppingListService.addIngredient(ingredient);
   }
-  ngOnInit() { }
+
+  ngOnInit() { 
+    console.log('divyaraj makwana');
+  }
 }
