753dacb28e2729c4143ed98822793d2c1cc97b5e
Divyaraj Makwana <divyaraj.makwana9425@gmail.com>
2024-04-09 11:28:08 +0530

    shopping list form done with td approach and edit and delete works fine

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Ingredients } from '../../shared/ingredients.model';
 import { ShoppingListService } from '../shopping-list.service';
 import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -9,17 +10,57 @@ import { NgForm } from '@angular/forms';
   styleUrl: './shopping-edit.component.css'
 })
 export class ShoppingEditComponent implements OnInit {
-
+  @ViewChild('f') slform: NgForm;
+  editedItemIndex: number
+  editMode = false
+  editedIngredient: Ingredients
+  subscription: Subscription
   constructor(private shoppingListService: ShoppingListService) { }
 
   onAddItem(form: NgForm) {
     const value = form.value
     console.log(value);
     const ingredient = new Ingredients(value.name, value.amount);
-    this.shoppingListService.addIngredient(ingredient);
+    if(this.editMode){
+      this.shoppingListService.updateIngredient(this.editedItemIndex,ingredient)
+    }else{
+      this.shoppingListService.addIngredient(ingredient);
+    }
+    this.editMode = false
+    form.reset()
   }
 
-  ngOnInit() { 
-    console.log('divyaraj makwana');
+  onClear(){
+    if(this.editMode){
+      if(confirm('You want to edit?')){
+        this.slform.reset({
+          name: this.editedIngredient.name,
+        })
+      }else{
+        this.slform.reset()
+        this.editMode = false
+      }
+    }else{
+      this.slform.reset()
+    }
+  }
+
+  onDelete(){
+    this.shoppingListService.deleteIngredient(this.editedItemIndex)
+    this.onClear()
+  }
+
+  ngOnInit() {
+    this.shoppingListService.startedEditing.subscribe(
+      (index: number) => {
+        this.editedItemIndex = index
+        this.editMode = true
+        this.editedIngredient = this.shoppingListService.getIngredient(index)
+        this.slform.setValue({
+          name: this.editedIngredient.name,
+          amount: this.editedIngredient.amount
+        })
+      }
+    )
   }
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -3,21 +3,34 @@ import { Subject } from "rxjs";
 
 export class ShoppingListService {
     IngredientsChanged = new Subject<Ingredients[]>();
+    startedEditing = new Subject<number>()
     Ingredients: Ingredients[] = [
         new Ingredients('Apples', 5),
         new Ingredients('Tomatoes', 10)
-      ]
+    ]
 
-      getIngredients(){
-          return this.Ingredients.slice();
-      }
+    getIngredients() {
+        return this.Ingredients.slice();
+    }
 
-      addIngredient(ingredient: Ingredients){
-          this.Ingredients.push(ingredient);
-          this.IngredientsChanged.next(this.Ingredients.slice());
-      }
-      addIngredients(ingredients: Ingredients[]){
-          this.Ingredients.push(...ingredients);
-          this.IngredientsChanged.next(this.Ingredients.slice());
-      }
+    getIngredient(index: number) {
+        return this.Ingredients[index];
+    }
+
+    addIngredient(ingredient: Ingredients) {
+        this.Ingredients.push(ingredient);
+        this.IngredientsChanged.next(this.Ingredients.slice());
+    }
+    addIngredients(ingredients: Ingredients[]) {
+        this.Ingredients.push(...ingredients);
+        this.IngredientsChanged.next(this.Ingredients.slice());
+    }
+    updateIngredient(index: number, newIngredient: Ingredients) {
+        this.Ingredients[index] = newIngredient;
+        this.IngredientsChanged.next(this.Ingredients.slice());
+    }
+    deleteIngredient(index: number) {
+        this.Ingredients.splice(index, 1);
+        this.IngredientsChanged.next(this.Ingredients.slice());
+    }
 }
