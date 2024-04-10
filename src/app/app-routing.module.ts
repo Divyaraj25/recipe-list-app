9b40542aa6506fbafe572e20b4e1618d6d0ab177
Divyaraj Makwana <divyaraj.makwana9425@gmail.com>
2024-04-10 14:54:17 +0530

    fixed an error of refresh in recipe page, resolver is added that do fetchrecipes when route changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,15 +5,18 @@ import { RouterModule, Routes } from "@angular/router";
 import { RecipeStartComponent } from "./recipes/recipe-start/recipe-start.component";
 import { RecipeDetailsComponent } from "./recipes/recipe-details/recipe-details.component";
 import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
+import { RecipeResolverService } from "./recipes/recipe-resolver.service";
 
 const appRoutes: Routes = [
     { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-    { path: 'recipes', component: RecipesComponent, children:[
-        { path:'', component:RecipeStartComponent },
-        { path:'new', component:RecipeEditComponent},
-        { path:':id', component:RecipeDetailsComponent},
-        { path:':id/edit', component:RecipeEditComponent}
-    ] },
+    {
+        path: 'recipes', component: RecipesComponent, children: [
+            { path: '', component: RecipeStartComponent },
+            { path: 'new', component: RecipeEditComponent, resolve: [RecipeResolverService] },
+            { path: ':id', component: RecipeDetailsComponent, resolve: [RecipeResolverService] },
+            { path: ':id/edit', component: RecipeEditComponent, resolve: [RecipeResolverService] }
+        ]
+    },
     { path: 'shopping-list', component: ShoppingListComponent }
 ];
 
