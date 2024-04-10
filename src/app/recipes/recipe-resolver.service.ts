a1540ad64c260a13a451106d7365f85fecf6c6bb
Divyaraj Makwana <divyaraj.makwana9425@gmail.com>
2024-04-10 15:02:18 +0530

    not sending per recipe request while fetching details in the page

--- src/app/recipes/recipe-resolver.service.ts
+++ src/app/recipes/recipe-resolver.service.ts
@@ -3,13 +3,20 @@ import { Recipe } from "./recipe.model";
 import { DataStorageService } from "../shared/data-storage.service";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
+import { recipeService } from "./recipe.service";
 
 @Injectable({ providedIn: 'root' })
 export class RecipeResolverService implements Resolve<Recipe[]> {
-    constructor(private dataStorageService: DataStorageService) { }
+    constructor(private dataStorageService: DataStorageService, private recipeService: recipeService) { }
 
     resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot){
+        const recipes = this.recipeService.getRecipes()
+
+        if(recipes.length === 0){
+            return this.dataStorageService.fetchRecipes()
+        }else{
+            return recipes
+        }
         // const recipes = this.dataStorageService.fetchRecipes()
-        return this.dataStorageService.fetchRecipes()
     }
 }
