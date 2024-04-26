3ede6aabf36dcae69f631fbe8a3c21d4d4d8f0da
Divyaraj Makwana <divyaraj.makwana9425@gmail.com>
2024-04-26 14:49:58 +0530

    added guard and authentication done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,11 +7,15 @@ import { RecipeDetailsComponent } from "./recipes/recipe-details/recipe-details.
 import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
 import { RecipeResolverService } from "./recipes/recipe-resolver.service";
 import { AuthComponent } from "./auth/auth.component";
+import { AuthGuard } from "./auth/auth.guard";
 
 const appRoutes: Routes = [
     { path: '', redirectTo: '/recipes', pathMatch: 'full' },
     {
-        path: 'recipes', component: RecipesComponent, children: [
+        path: 'recipes', 
+        component: RecipesComponent, 
+        canActivate: [AuthGuard],
+        children: [
             { path: '', component: RecipeStartComponent },
             { path: 'new', component: RecipeEditComponent, resolve: [RecipeResolverService] },
             { path: ':id', component: RecipeDetailsComponent, resolve: [RecipeResolverService] },
